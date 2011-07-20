7516888fa07894b69f0c50f54fc80c5e748250bc
Esau Rodriguez <esauro@gmail.com>
2011-07-20 19:17:08 +0100

    Retab and the first model method!

--- simpleblog/posts/models.py
+++ simpleblog/posts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Post(models.Model):
-	title = models.CharField(max_length = 200)
-	published_date = models.DateTimeField(auto_now_add = datetime.datetime.now())
-	text = models.TextField()
-	author = models.ForeignKey(User)
+    title = models.CharField(max_length = 200)
+    published_date = models.DateTimeField(auto_now_add = datetime.datetime.now())
+    text = models.TextField()
+    author = models.ForeignKey(User)
+
+    def publicado_hoy(self):
+        return self.fecha.date() == datetime.date.today()
