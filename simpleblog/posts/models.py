add84b7aafccc71e281c0f84d926443e1000d06b
Esau Rodriguez <esauro@gmail.com>
2011-07-20 19:13:38 +0100

    Now we have the author too!

--- simpleblog/posts/models.py
+++ simpleblog/posts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length = 200)
 	published_date = models.DateTimeField(auto_now_add = datetime.datetime.now())
 	text = models.TextField()
+	author = models.ForeignKey(User)
