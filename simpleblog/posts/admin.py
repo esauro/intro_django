4df1b19d0a82a5935079ada9a1aa25d9298c0500
Esau Rodriguez <esauro@gmail.com>
2011-07-20 20:30:46 +0100

    Some cool staff in admin

--- simpleblog/posts/admin.py
+++ simpleblog/posts/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from models import Post
 
-admin.site.register(Post)
+def make_published(modeladmin, request, queryset):
+    queryset.update(published=True)
+make_published.short_description = "Publicar los siguientes posts"
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'published_date'
+    list_display = ('title', 'author', 'published_date', 'publicado_hoy', 'published')
+    list_filter = ('author', 'published')
+    search_fields = ['title', 'text']
+    actions = [make_published,]
+
+
+admin.site.register(Post, PostAdmin)
