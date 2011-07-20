a6c52f5efb5aa78869b187898c0ff3e385c8c8a4
Esau Rodriguez <esauro@gmail.com>
2011-07-20 19:12:24 +0100

    The first model.

--- simpleblog/posts/models.py
+++ simpleblog/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length = 200)
+	published_date = models.DateTimeField(auto_now_add = datetime.datetime.now())
+	text = models.TextField()
