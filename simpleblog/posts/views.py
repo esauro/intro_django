5e66eba06580d6063dc0c9237028858f24c7a0b1
Esau Rodriguez <esauro@gmail.com>
2011-07-20 19:58:47 +0100

    This version is the first version that works!

--- simpleblog/posts/views.py
+++ simpleblog/posts/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from models import Post
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def list_posts(request):
     posts = Post.objects.all().order_by('-published_date')
@@ -8,4 +8,4 @@ def list_posts(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk = post_id)
-    return render_to_response("posts/post_detail.html", {'post': posts})
+    return render_to_response("posts/post_detail.html", {'post': post})
--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'simpleblog.views.home', name='home'),
     url(r'^list_posts/$', 'posts.views.list_posts', name="list_posts"),
-    url(r'^post_detail/$', 'posts.views.post_detail', name="post_detail"),
+    url(r'^post_detail/(?P<post_id>\d+)/$', 'posts.views.post_detail', name="post_detail"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
